0e81a72b721cf0f7cd70a2ce13825de228889fde
thejjwang <jonathanjacobwang@gmail.com>
2023-08-29 10:13:57 -0700

    header done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "./Header"
+import Input from "./Input"
+import Display from "./Display"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Input />
+      <Display />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return (
+        <div className="heading">
+            <h1>Molar Mass Calculator</h1>
+        </div>
+    )
+}
+
+export default Header;
